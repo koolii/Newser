d4122df8986630ad9bc818d3143f2c84bffa8cc9
kuriya0909 <ipod0909goo@gmail.com>
2014-09-14 02:05:27 +0900

    add xdomain ajax

--- js/script.js
+++ js/script.js
@@ -4,21 +4,12 @@ $(function() {
 	$.ajax({
 		type: 'GET',
 		url: 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-&query=isai',
-		// crossDomain: true,
-		dataType : "jsonp",
-		success: function(result) {
-			console.log("success: " + result);
-		},
-		error: function(error) {
-			console.log("error: " + error);
-		}
-	})
-	// .done(function(result) {
-	// 	console.log(result);
-
-	// }).fail(function(msg){
-	// 	alert(msg);
-	// }).complete(function(){
-	// 	console.warn("complete!!");
-	// });
+		crossDomain: true,
+	}).done(function(result) {
+		console.log(result);
+	}).error(function(msg){
+		alert(msg);
+	}).complete(function(){
+		console.warn("complete!!");
+	});
 });
