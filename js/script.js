c1b3cd88c2f91aab689cda642068782b7ab21c29
kuriya0909 <ipod0909goo@gmail.com>
2014-09-14 02:38:16 +0900

    create Ajax Object*

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
 $(function() {
 	console.log("test");
 
-	$.ajax({
-		type: 'GET',
-		url: 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-&query=isai',
-		crossDomain: true,
-	}).done(function(result) {
-		console.log(result);
-	}).error(function(msg){
-		alert(msg);
-	}).complete(function(){
-		console.warn("complete!!");
-	});
+	var Ajax = {
+		execute: function(keyword) {
+			var $def = $.Defferd();
+			var url = this.createUrl(keyword);
+
+			$.ajax({
+				type: 'GET',
+				url: url,
+				crossDomain: true,
+			}).done(function(result) {
+				console.log(result);
+
+				$def.resolve(this.callBack(result));
+			}).error(function(msg){
+				alert(msg);
+				$def.reject();
+			}).complete(function(){
+				console.warn("complete!!");
+			});
+
+			return $def.promise();
+		},
+
+		createUrl: function(keyword) {
+			var baseUrl = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-';
+			return baseUrl + '&query=' + keyword;
+
+		},
+		callBack: function(result) {
+			var html = $.parseHTML(result.results[0]);
+			var $pElement = $(html[5]);
+
+			return $.parseJSON($pElement.text());
+		}
+	};
+
+	$.when(Ajax.execute("isai"))
+		.done(function(result) {
+			console.log("Success Ajax: " + result);
+		});
+
 });
