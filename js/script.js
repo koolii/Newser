8cf2ccdfb272277d202313dbcd25151447e779b4
kuriya0909 <ipod0909goo@gmail.com>
2014-09-13 22:22:28 +0900

    add ajax test　of yahoo"

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(function() {
 	console.log("test");
+	var call = "callbackTest";
 
 	$.ajax({
 		type: 'GET',
 		url: 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-&query=isai',
 		// crossDomain: true,
 		dataType : "jsonp",
+		jsonp: call
 	}).done(function(result) {
 		console.log(result);
-	}).error(function(msg){
+		
+	}).fail(function(msg){
 		alert(msg);
 	}).complete(function(){
 		console.warn("complete!!");
