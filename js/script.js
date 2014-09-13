5a83309a590eb27eea1e2909d9fd63e18c1969ac
kuriya0909 <ipod0909goo@gmail.com>
2014-09-13 21:52:24 +0900

    add ajax test　of yahoo"

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function() {
 
 	$.ajax({
 		type: 'GET',
-		url: 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-&query=%E8%AE%83%E5%B2%90%E3%81%86%E3%81%A9%E3%82%93',
+		url: 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?appid=dj0zaiZpPXEwME0wemIxUDVYMCZzPWNvbnN1bWVyc2VjcmV0Jng9Yzc-&query=讃岐うどん',
 		crossDomain: true		
 	}).done(function(result) {
 		console.log(result);
